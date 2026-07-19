c93a2557f9eaa8af6d3a20aa065b4262519e6a07
musesun727 <musesun_727@outlook.com>
2026-07-19 16:33:41 +0800

    Site updated: 2026-07-19 16:33:40

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1784449135380";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1784450019032";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
